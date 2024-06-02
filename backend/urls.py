c6e1acb4496148ea9ab556ff50725e5d98273cca
arcboomi <arcboomi@gmail.com>
2024-06-02 20:09:10 +0100

    Adding Swagger

--- backend/urls.py
+++ backend/urls.py
@@ -16,8 +16,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Backend Computers APIS",
+        default_version='1.0.0',
+        description="This is swagger for our apis."
+
+    ),
+    public=True,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('solutions.urls'))
+    path('', include('solutions.urls')),
+    path('swagger/schema/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-schema'),
 ]
